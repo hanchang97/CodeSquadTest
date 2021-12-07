1a24998dd472b8bced4aa2b16ee268d1e8f73040
hanchang97 <nimok97@gmail.com>
2021-12-07 19:51:55 +0900

    Feat : Create Map, ReadMap, MapStorage class
    클래스 생성
    Map : 스테이지 맵의 주요 속성들 포함
    ReadMap : 입력으로 주어진 문자열을 스테이지 별로 구분
    MapStorage : 스테이지 별로 맵을 포함

--- step2/src/Step2Test.kt
+++ step2/src/Step2Test.kt
@@ -1,6 +1,73 @@
 fun main(args: Array<String>){
 
+    var target = "Stage 1\n" +
+            "#####\n" +
+            "#OoP#\n" +
+            "#####\n" +
+            "=====\n" +
+            "Stage 2\n" +
+            "  #######\n" +
+            "###  O  ###\n" +
+            "#    o    #\n" +
+            "# Oo P oO #\n" +
+            "###  o  ###\n" +
+            " #   O  #\n" +
+            " ########"
+
+    var changeTarget = target.split("\n")
+
+    var mapStorage = MapStorage()
+
+    var readMap = ReadMap(changeTarget, mapStorage)
+    readMap.convertMap()
+}
 
 
+class Map(var mapValueList : ArrayList<String>, var stageNum: Int){
 
+
+}
+
+
+class ReadMap(var mapLineList : List<String>, var mapStorage: MapStorage){
+
+    fun convertMap(){
+
+        var currentStage = 1
+        var mapValueList = ArrayList<String>()
+
+        for(i in 0..mapLineList.size-1){
+
+            var line = mapLineList[i]
+
+            if(line.contains("Stage")){
+                println(currentStage++)
+            }
+            else if(line.contains("=")){
+                var map = Map(mapValueList, currentStage)
+                mapStorage.addMap(map)
+                mapValueList.clear()
+            }
+            else if(i == mapLineList.size-1){
+                mapValueList.add(line)
+                var map = Map(mapValueList, currentStage)
+                mapStorage.addMap(map)
+
+            }
+            else{
+                mapValueList.add(line)
+            }
+
+        }
+    }
+
+}
+
+class MapStorage(){
+
+    private var mapList = ArrayList<Map>()
+
+    fun addMap(map: Map){
+        mapList.add(map)
+    }
 }
