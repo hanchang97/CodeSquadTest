a81f7b0467d50a91ec1f144f86594c6e19305200
hanchang97 <nimok97@gmail.com>
2021-12-08 01:00:48 +0900

    Refactor : step2/ Modify SetGame, PlayGame class
    SetGame : 플레이어 초기 위치를 찾고 PlayGame에 위치 값을 보내도록 수정,
    맵 상의 구멍이 덮여있는 상태인지 검사하는 배열 추가
    PlayGame : 플레이어 이동시 플레이어 위치 값 갱신, 구멍의 덮힘 여부 체크 수정

--- step2/src/Step2Test.kt
+++ step2/src/Step2Test.kt
@@ -159,6 +159,12 @@ class SetGame(){
                 }
             }
 
+            // 구멍이 무언가에 덮여있는 상태인지 검사하기 위함
+            var holeCoveredCheckArray = Array(map.mapHeight, {Array(map.mapWidth, {false})})
+
+            // 초기 플레이어 위치 찾기
+            var playerLocation = findPlayerLocation(temp)
+
             while(true) {
                 print("SOKOBAN> ")
                 var command = readLine()
@@ -167,33 +173,12 @@ class SetGame(){
                     println("no command : Bye~")
                     System.exit(0)
                 } else {
-                    command = command!!.lowercase()  // 소문자 변환 / 문제의 입력에 대문자가 주어지는 경우 존재
                     println(command)
                     println()
-                    PlayGame.playGame(temp, command!!)
+                    PlayGame.playGame(playerLocation, temp, holeCoveredCheckArray, command!!)
                 }
             }
 
-        }
-    }
-}
-
-class PlayGame(){
-    companion object{
-        fun playGame(map : Array<Array<Int>>, command : String){
-            var playerLocation = findPlayerLocation(map)
-
-            println("플레이어 위치 : ${playerLocation.playerY}, ${playerLocation.playerX}")
-
-            for(i in 0..command.length-1){
-                if(command[i] == 'q'){  //종료
-                    println("Bye~")
-                    System.exit(0)
-                }
-                else{
-
-                }
-            }
         }
 
         fun findPlayerLocation(map: Array<Array<Int>>) : PlayerLocation{
@@ -207,11 +192,197 @@ class PlayGame(){
 
             return PlayerLocation(0, 0)
         }
+    }
+}
+
+class PlayGame(){
+    companion object{
+        fun playGame(playerLocation : PlayerLocation, map : Array<Array<Int>>,
+                     holeCoveredCheckArray : Array<Array<Boolean>>,  command : String){
+
+            println("플레이어 위치 : ${playerLocation.playerY}, ${playerLocation.playerX}")
+
+            for(i in 0..command.length-1){
+                if(command[i] == 'q'){  //종료
+                    println("Bye~")
+                    System.exit(0)
+                }
+                else if(command[i] == 'w' || command[i] == 'a' || command[i] == 's' || command[i] == 'd'
+                    || command[i] == 'W' || command[i] == 'A' || command[i] == 'S' || command[i] == 'D'){
+
+                    movePlayer(playerLocation, map, holeCoveredCheckArray, command[i])
+                }
+                else{  // 잘못된 명령어 = w,a,s,d,q에 어느것에도 해당하지 않음
+                    printCurrentMap(map)
+                    println("${command[i]}: (경고!) 해당 명령을 수행할 수 없습니다!")
+                }
+            }
+        }
+
+
+        fun movePlayer(playerLocation: PlayerLocation, map : Array<Array<Int>>,
+                       holeCoveredCheckArray : Array<Array<Boolean>>, commandChar : Char){
+
+            var playerY = playerLocation.playerY
+            var playerX = playerLocation.playerX
+
+            if(commandChar == 'w' || commandChar == 'W'){  //위쪽
+                if(map[playerY-1][playerX] == 5 || map[playerY-1][playerX] == 1){
+                    // 플레이어 현재 위치 값 변화
+                    if(holeCoveredCheckArray[playerY][playerX]){ // 현재 플레이어 위치에 구멍이 있었다면
+                        map[playerY][playerX] = 1  // 플레이어가 이동했으므로 구멍이 다시 보여야 하므로 1 할당
+                        holeCoveredCheckArray[playerY][playerX] = false // 구멍이 다시 보이므로 false로 변경
+                    }
+                    else{ // 구멍 없었다면 현재 위치는 공백이 된다
+                        map[playerY][playerX] = 5
+                    }
+
+                    //플레이어 다음 위치 값 변화
+                    if(map[playerY-1][playerX] == 1){  // 다음 위치가 구멍을 덮게 되는 위치
+                        map[playerY-1][playerX] = 3
+                        holeCoveredCheckArray[playerY-1][playerX] = true  // 구멍이 덮였으므로 true 된다
+                    }
+                    else{ // 공백으로 이동
+                        map[playerY-1][playerX] = 3
+                    }
+
+                    printCurrentMap(map)
+                    println("${commandChar}: 위쪽으로 이동합니다.")
+
+
+                    playerLocation.playerY -= 1  // 플레이어 위치 업데이트
+
+                }
+                else{
+                    printCurrentMap(map)
+                    println("${commandChar}: (경고!) 해당 명령을 수행할 수 없습니다!")
+                }
+            }
+            else if(commandChar == 'a' || commandChar == 'A'){  //왼쪽
+                if(map[playerY][playerX-1] == 5 || map[playerY][playerX-1] == 1){
+                    // 플레이어 현재 위치 값 변화
+                    if(holeCoveredCheckArray[playerY][playerX]){ // 현재 플레이어 위치에 구멍이 있었다면
+                        map[playerY][playerX] = 1  // 플레이어가 이동했으므로 구멍이 다시 보여야 하므로 1 할당
+                        holeCoveredCheckArray[playerY][playerX] = false // 구멍이 다시 보이므로 false로 변경
+                    }
+                    else{ // 현재 위치에 구멍 없었다면 현재 위치는 공백이 된다
+                        map[playerY][playerX] = 5
+                    }
+
+                    //플레이어 다음 위치 값 변화
+                    if(map[playerY][playerX-1] == 1){  // 다음 위치가 구멍을 덮게 되는 위치
+                        map[playerY][playerX-1] = 3
+                        holeCoveredCheckArray[playerY][playerX-1] = true  // 구멍이 덮였으므로 true 된다
+                    }
+                    else{ // 공백으로 이동
+                        map[playerY][playerX-1] = 3
+                    }
+
+                    printCurrentMap(map)
+                    println("${commandChar}: 왼쪽으로 이동합니다.")
+
+                    playerLocation.playerX -= 1  // 플레이어 위치 업데이트
+
+                }
+                else{
+                    printCurrentMap(map)
+                    println("${commandChar}: (경고!) 해당 명령을 수행할 수 없습니다!")
+                }
+
+            }
+            else if(commandChar == 's' || commandChar ==  'S'){  //아래쪽
+                if(map[playerY+1][playerX] == 5 || map[playerY+1][playerX] == 1){
+                    // 플레이어 현재 위치 값 변화
+                    if(holeCoveredCheckArray[playerY][playerX]){ // 현재 플레이어 위치에 구멍이 있었다면
+                        map[playerY][playerX] = 1  // 플레이어가 이동했으므로 구멍이 다시 보여야 하므로 1 할당
+                        holeCoveredCheckArray[playerY][playerX] = false // 구멍이 다시 보이므로 false로 변경
+                    }
+                    else{ // 현재 위치에 구멍 없었다면 현재 위치는 공백이 된다
+                        map[playerY][playerX] = 5
+                    }
+
+                    //플레이어 다음 위치 값 변화
+                    if(map[playerY+1][playerX] == 1){  // 다음 위치가 구멍을 덮게 되는 위치
+                        map[playerY+1][playerX] = 3
+                        holeCoveredCheckArray[playerY+1][playerX] = true  // 구멍이 덮였으므로 true 된다
+                    }
+                    else{ // 공백으로 이동
+                        map[playerY+1][playerX] = 3
+                    }
+
+                    printCurrentMap(map)
+                    println("${commandChar}: 아래쪽으로 이동합니다.")
+
+                    playerLocation.playerY += 1  // 플레이어 위치 업데이트
+                }
+                else{
+                    printCurrentMap(map)
+                    println("${commandChar}: (경고!) 해당 명령을 수행할 수 없습니다!")
+                }
+            }
+            else{ // d or D 인 경우  //오른쪽
+                if(map[playerY][playerX+1] == 5 || map[playerY][playerX+1] == 1){
+                    // 플레이어 현재 위치 값 변화
+                    if(holeCoveredCheckArray[playerY][playerX]){ // 현재 플레이어 위치에 구멍이 있었다면
+                        map[playerY][playerX] = 1  // 플레이어가 이동했으므로 구멍이 다시 보여야 하므로 1 할당
+                        holeCoveredCheckArray[playerY][playerX] = false // 구멍이 다시 보이므로 false로 변경
+                    }
+                    else{ // 현재 위치에 구멍 없었다면 현재 위치는 공백이 된다
+                        map[playerY][playerX] = 5
+                    }
+
+                    //플레이어 다음 위치 값 변화
+                    if(map[playerY][playerX+1] == 1){  // 다음 위치가 구멍을 덮게 되는 위치
+                        map[playerY][playerX+1] = 3
+                        holeCoveredCheckArray[playerY][playerX+1] = true  // 구멍이 덮였으므로 true 된다
+                    }
+                    else{ // 공백으로 이동
+                        map[playerY][playerX+1] = 3
+                    }
+
+                    printCurrentMap(map)
+                    println("${commandChar}: 오른쪽으로 이동합니다.")
+
+                    playerLocation.playerX += 1  // 플레이어 위치 업데이트
+                }
+                else{
+                    printCurrentMap(map)
+                    println("${commandChar}: (경고!) 해당 명령을 수행할 수 없습니다!")
+                }
+            }
+        }
+
+        fun printCurrentMap(map: Array<Array<Int>>){
+            println()
+            for(i in 0..map.size-1){
+                for(j in 0..map[i].size-1){
+                    when(map[i][j]){
+                        0 -> print('#')
+                        1 -> print('O')
+                        2 -> print('o')
+                        3 -> print('P')
+                        5 -> print(' ')
+                    }
+                }
+                println()
+            }
+            println()
+
+            for(i in 0..map.size-1){
+                for(j in 0..map[i].size-1){
+                    print(map[i][j])
+                    }
+                println()
+                }
+                println()
+            }
+        }
 
     }
 
 
-}
+
+
 
 // 인덱스 기준 값
 data class PlayerLocation(
