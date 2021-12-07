70262758d8965d0ea04352584cac812b3cf7bf4d
hanchang97 <nimok97@gmail.com>
2021-12-07 21:02:11 +0900

    Refactor : modify Map, MapStorage class
    Map : 정수형 배열로 맵을 저장하는 메서드 추가,현재 단계와 맵 정보를 출력하는 메서드 추가
    MapStorage : 맵 리스트에 맵 추가 시 메서드 수정, 원하는 스테이지 입력 받아 해당 스테이지 맵 사용할 수 있게 구현

--- step2/src/Step2Test.kt
+++ step2/src/Step2Test.kt
@@ -20,20 +20,64 @@ fun main(args: Array<String>){
 
     var readMap = ReadMap(changeTarget, mapStorage)
     readMap.convertMap()
+
+    mapStorage.startGame(2)
 }
 
 
 class Map(var mapValueList : ArrayList<String>, var stageNum: Int){
 
+    var mapIntArray = ArrayList<ArrayList<Int>>()
 
+    fun converToIntArray(){
+        for(i in 0..mapValueList.size-1){
+            var line = mapValueList[i]
+
+            var lineIntList = ArrayList<Int>()
+
+            for(j in 0..line.length-1){
+                when(line[j]){
+                    '#' -> lineIntList.add(0)
+                    'O' -> lineIntList.add(1)
+                    'o' -> lineIntList.add(2)
+                    'P' -> lineIntList.add(3)
+                    '=' -> lineIntList.add(4)
+                    ' ' -> lineIntList.add(5)
+                }
+            }
+
+            mapIntArray.add(lineIntList)
+        }
+
+
+    }
+
+    fun printStage(){
+        println("Stage "+ stageNum)
+        println()
+    }
+
+    fun printMap(){
+        for(i in 0..mapIntArray.size-1){
+            for(j in 0..mapIntArray[i].size-1){
+                when(mapIntArray[i][j]){
+                    0 -> print('#')
+                    1 -> print('O')
+                    2 -> print('o')
+                    3 -> print('P')
+                    5 -> print(' ')
+                }
+            }
+            println()
+        }
+    }
 }
 
 
 class ReadMap(var mapLineList : List<String>, var mapStorage: MapStorage){
 
     fun convertMap(){
-
-        var currentStage = 1
+        var currentStage = 0
         var mapValueList = ArrayList<String>()
 
         for(i in 0..mapLineList.size-1){
@@ -41,7 +85,7 @@ class ReadMap(var mapLineList : List<String>, var mapStorage: MapStorage){
             var line = mapLineList[i]
 
             if(line.contains("Stage")){
-                println(currentStage++)
+                currentStage++
             }
             else if(line.contains("=")){
                 var map = Map(mapValueList, currentStage)
@@ -69,5 +113,21 @@ class MapStorage(){
 
     fun addMap(map: Map){
         mapList.add(map)
+        map.converToIntArray()
+    }
+
+    fun startGame(stage: Int){
+        GameStart.startGame(mapList[stage-1])
+    }
+}
+
+class GameStart(){
+
+    companion object{
+        fun startGame(map: Map){
+            map.printStage()
+            map.printMap()
+
+        }
     }
 }
